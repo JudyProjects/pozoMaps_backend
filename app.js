fccbba6bbec2c67e2b39bd142c971c763a8ac235
Franco Sancristobal <franco.sancris29@gmail.com>
2024-10-10 20:11:50 -0300

    Fix initial commit

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ var cookieParser = require('cookie-parser');
 global.__root = __dirname + '/';
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
+var cors = require('cors');
 
+app.use(cors());
 app.use(cookieParser());
 app.use('/socket', express.static(path.join(__dirname, 'node_modules/socket.io')));
 
+app.get('/', function (req, res) {
+    res.status(200).send('Conexión con server funcionando');
+});
 module.exports = { server, express };
 var socket = require('./socket.js');
