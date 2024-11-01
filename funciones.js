67e665481f90f19db1f3def8bb768bd34c7dee48
Angelo <angelofestino5@gmail.com>
2024-10-31 22:34:51 -0300

    [fixMongoCrearUsuario]
    - Modularizacion para operacion mongo

--- funciones.js
+++ funciones.js
@@ -1,3 +1,17 @@
 var mongoose = require("mongoose");
+var User = require('./models/User.model.js');
+const bcrypt = require('bcryptjs');
 
-module.exports = {};
+async function crearUsuario(name, email, password) {
+    // Crear nuevo usuario si no existe
+		var hashedPassword = bcrypt.hashSync(password, 8);
+		const user = await User.create({
+			name: name,
+			email: email,
+			password: hashedPassword
+		});
+        return await user.save();
+}
+module.exports = {
+    crearUsuario,
+};
